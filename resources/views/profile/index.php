9c5dc4505ab82fd6fb119dbcd71d9c25a2a6e8c4
markeze207 <markezeafu@gmail.com>
2023-02-19 13:21:58 +0300

    changed profile view

--- resources/views/profile/index.php
+++ resources/views/profile/index.php
@@ -5,9 +5,16 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
     <title>Document</title>
 </head>
 <body>
-    Приветствую, <?=$userName?>
+    <div class="card">
+        <div class="card-header">Добро пожаловать!, <?=$userName?></div>
+        <div class="card-body">
+            <h5 class="card-title">Вы вошли в систему</h5>
+        </div>
+    </div>
 </body>
 </html>
